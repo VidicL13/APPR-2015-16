9b324f9839f8f76896870da4f2da9149265ff664
Luka Vidic <luka94vidic@gmail.com>
2015-12-05 23:11:01 +0100

    Pričel sem s pisanjem poročila, še ni dokončano, posebaj del z uvozom in nadaljevanje. Napravit moram True/False vektor, ki mi bo pravilno kreiral data.frame za nadaljne grafe

---
+++ lib/knjiznice.R
@@ -0,0 +1,5 @@
+# Naložimo potrebne knjižnice, če še niso za delo na projektu
+packages <- c("ggplot2", "dplyr", "quantmod", "gtable")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))  
+}
