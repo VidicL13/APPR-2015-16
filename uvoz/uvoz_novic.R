68696aa0d6672a35d5445dcc68868dad2a97435b
Luka Vidic <luka94vidic@gmail.com>
2015-12-23 11:08:53 +0100

    čistim svoje okolje, ker imam precej nereda

--- uvoz/uvoz_novic.R
+++ uvoz/uvoz_novic.R
@@ -8,7 +8,7 @@ prebrano <- read_html(naslov, encoding = "UTF-8")
 html_tabela <- prebrano %>% html_node(xpath = '//*[@id="fcc1_dgrC"]')
 tabela <- html_tabela %>% html_table(header = TRUE)
 Oil <- grep("^.*DOE Crude.*$",tabela[,4])
-Oil <- 72
+Oil <- Oil+1
 
 # prebrano <- read_html(naslov, encoding = "UTF-8")
 # html_tabela <- prebrano %>% html_node(xpath = '//*[@id="fcc1_dgrC"]')
@@ -54,7 +54,7 @@ for (i in 1:12){
 tabela2[,1] <- gsub("^(\\d{2})\\s(\\d{2}),\\s(\\d{4})","\\3-\\1-\\2",tabela2[,1])
 tabela2[,2] <- gsub(":","-", tabela2[,2])
 
-tabela2 <- tabela2[c(-105),]
+tabela2 <- tabela2[c(-105,-262),]
 
 for (i in 1:(floor(length(tabela2[,1])/4))){
   z <- 4*(i-1)+1
@@ -64,7 +64,48 @@ for (i in 1:(floor(length(tabela2[,1])/4))){
   tabela2[z+3,1] <- tabela2[z,1]
 }
 
-# tabela2[,1] <- t
+tabela2[,1] <- paste(tabela2[,1],tabela2[,2], sep  = "-")
+tabela2 <- tabela2[,c(-2)]
+names(tabela2) <- c("date", "which news", "actual", "forcast", "neki")
+tabela2 <- tabela2[c(-1,-2,-3,-4),]
+tabela2 <- tabela2[,c(-5)]
+tabela2$difference <- as.numeric(tabela2$actual) - as.numeric(tabela2$forcast)
+tabela2$differenceprocent <- as.numeric(tabela2$actual)/as.numeric(tabela2$forcast)*100
+
+tab <- tabela2
+tab1 <- data.frame("date" = rep(NA,floor(length(tab[,1])/4)),
+                   "which news" = rep(NA,floor(length(tab[,1])/4)),
+                   "actual" = rep(NA,floor(length(tab[,1])/4)),
+                   "forcast" = rep(NA,floor(length(tab[,1])/4)),
+                   "difference" = rep(NA,floor(length(tab[,1])/4)))
+tab2 <- data.frame("date" = rep(NA,floor(length(tab[,1])/4)),
+                   "which news" = rep(NA,floor(length(tab[,1])/4)),
+                   "actual" = rep(NA,floor(length(tab[,1])/4)),
+                   "forcast" = rep(NA,floor(length(tab[,1])/4)),
+                   "difference" = rep(NA,floor(length(tab[,1])/4)))
+tab3 <- data.frame("date" = rep(NA,floor(length(tab[,1])/4)),
+                   "which news" = rep(NA,floor(length(tab[,1])/4)),
+                   "actual" = rep(NA,floor(length(tab[,1])/4)),
+                   "forcast" = rep(NA,floor(length(tab[,1])/4)),
+                   "difference" = rep(NA,floor(length(tab[,1])/4)))
+tab4 <- data.frame("date" = rep(NA,floor(length(tab[,1])/4)),
+                   "which news" = rep(NA,floor(length(tab[,1])/4)),
+                   "actual" = rep(NA,floor(length(tab[,1])/4)),
+                   "forcast" = rep(NA,floor(length(tab[,1])/4)),
+                   "difference" = rep(NA,floor(length(tab[,1])/4)))
+for (i in 1:floor(length(tab[,1])/4)){
+  z <- 4*(i-1)+1
+  tab1[i,] <- tab[z,]
+  tab2[i,] <- tab[z+1,]
+  tab3[i,] <- tab[z+2,]
+  tab4[i,] <- tab[z+3,]
+}
+
+DistillateInventories <- tab1
+CrudeOilInventories <- tab2
+GasolineInventories <- tab3
+RefineryUtilization <- tab4
+rm(tab4, tab3, tab2, tab1, tab)
 
 
 
